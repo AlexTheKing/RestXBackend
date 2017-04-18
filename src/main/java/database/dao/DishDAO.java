7a27b2cda3c6f926f9fc98a9dd17d4c85802a851
Alexandr Beitsiuk <Alexandr_Beitsiuk@epam.com>
2017-04-18 17:24:40 +0300

    Added .idea

--- src/main/java/database/dao/DishDAO.java
+++ src/main/java/database/dao/DishDAO.java
@@ -4,6 +4,7 @@ import database.DatabaseHandler;
 import database.validator.DishValidator;
 import model.Dish;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
 
 import java.util.List;
 
@@ -27,6 +28,16 @@ public class DishDAO {
 
     public boolean delete(Dish dish){
         return DatabaseHandler.run((Session session) -> session.delete(dish));
+    }
+
+    public Dish getByName(String name) {
+        final Dish[] dish = new Dish[1];
+        DatabaseHandler.run((Session session) -> {
+            Query query = session.createQuery("from Dish where name :paramName");
+            query.setParameter("paramName", name);
+            dish[0] = (Dish) query.list().get(0);
+        });
+        return dish[0];
     }
 
 }
